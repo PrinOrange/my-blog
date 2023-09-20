24c848b691eb51757ee2a3b3737a9aad71288f09
PrinOrange <prinorange@outlook.com>
2023-09-20 22:33:13 +0800

    connect to my notion account

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6a9f8c1606fc4ffbbb322e647b4d6870',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '但为君故',
+  domain: 'terminels.com',
+  author: 'terminels',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My blog for my life and thinkings about technology',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  twitter: 'terminels',
+  github: 'prinOrange',
+
+  mastodon: 'https://mas.to/@terminels', // optional mastodon profile URL, provides link verification
+  zhihu: 'https://www.zhihu.com/people/prinOrange',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -50,16 +51,15 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'ABOUT',
+      pageId: '630d0eb8d33348c9827aa9dc97717aac'
+    },
+    {
+      title: 'RESEARCH',
+      pageId: 'fa565740bf9e405ba88267660a607a79'
+    }
+  ]
 })
